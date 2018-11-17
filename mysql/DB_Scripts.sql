b5ace98aee0399a9c2c45b3eeac98963416d01f7
adarshnagaraj22 <adarshgowda4@gmail.com>
2018-11-17 13:27:53 +0530

    first commit

---
+++ mysql/DB_Scripts.sql
@@ -0,0 +1,12 @@
+use sample;
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `USER` (
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `regdate` varchar(255) NOT NULL); 
